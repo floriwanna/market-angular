63c1a4a4e44ffabad6e9fe424bdb9b525dfd1ab8
floriana <floriana.loiacono@wayops.eu>
2020-10-20 10:11:12 +0200

    Add both side login

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,67 @@
 const express = require('express');
+// const { reverse } = require('lodash');
+const parseJSON = require("body-parser").json();
+const bcrypt = require('bcrypt');
+const JWT = require('jsonwebtoken');
+function App(db, secret) {
+  let app = express();
 
-  function App(db,secret){
-    let app = express();
 
-    app.get('/',(req,res,next)=>{
-        res.write('Hi there!')
-        res.end();
+  const usersCollection = db.collection('users');
+  const customerCollection = db.collection('customer');
+
+  app.get('/', (req, res, next) => {
+    res.write('Hi there!')
+    res.end();
+  });
+
+  app.post('/admin/login', parseJSON, (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      res.statusCode(400)
+      res.end();
+    }
+    usersCollection.findOne({ username: req.body.username }, (err, doc) => {
+      if (err)
+        return next(err);
+      if (!doc) return next(new Error("invalid username"));
+      if (doc.deactivated) return next(new Error("user is deactivated"));
+
+      bcrypt.compare(req.body.password, doc.password, (err, verifies) => {
+        if (err) return next(err);
+        if (!verifies) return next(new Error("invalid password"));
+
+        let token = JWT.sign({
+          username: doc.username,
+          permissions: doc.permissions,
+        }, secret);
+        res.json(token);
+      })
     });
+  });
 
-    return app;
+  app.post("/login", parseJSON, (req, res, next) => {
+    if (!req.body.username || !req.body.password) {
+      res.statusCode(400)
+      res.end();
+    }
+    customerCollection.findOne({ username: req.body.username }, (err, doc) => {
+      if (err)
+        return next(err);
+      if (!doc) return next(new Error("invalid username"));
+      if (doc.deactivated) return next(new Error("user is deactivated"));
+
+      bcrypt.compare(req.body.password, doc.password, (err, verifies) => {
+        if (err) return next(err);
+        if (!verifies) return next(new Error("invalid password"));
+
+        let token = JWT.sign({
+          username: doc.username,
+        }, secret);
+        res.json(token);
+      })
+    });
+  })
+
+  return app;
 }
 module.exports = App;
