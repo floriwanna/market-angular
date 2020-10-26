7c99b48acb5382b8810f2b4b96a7b567239bf184
floriana <floriana.loiacono@wayops.eu>
2020-10-26 11:42:39 +0100

    add 'sign in' endpoint y metodo checkstructure

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const parseJSON = require("body-parser").json();
 const bcrypt = require('bcrypt');
 const JWT = require('jsonwebtoken');
+const { without } = require('lodash');
+const { ObjectID } = require('mongodb');
 function App(db, secret) {
   let app = express();
 
@@ -15,9 +17,21 @@ function App(db, secret) {
     res.end();
   });
 
+
+  const checkBodyStructure = function (body, args) {
+    if (Array.isArray(args)) {
+      for (let x of args) {
+        if (!body[x]) return false;
+      }
+      return true;
+    }
+    else
+      return !!body[args]
+  }
+
   app.post('/admin/login', parseJSON, (req, res, next) => {
     if (!req.body.username || !req.body.password) {
-      res.statusCode(400)
+      res.statusCode = 400;
       res.end();
     }
     usersCollection.findOne({ username: req.body.username }, (err, doc) => {
@@ -41,15 +55,14 @@ function App(db, secret) {
 
   app.post("/login", parseJSON, (req, res, next) => {
     if (!req.body.username || !req.body.password) {
-      res.statusCode(400)
-      res.end();
+      res.statusCode = 400
+      res.end(); return;
     }
     customerCollection.findOne({ username: req.body.username }, (err, doc) => {
       if (err)
         return next(err);
-      if (!doc) return next(new Error("invalid username"));
+      if (!doc || doc["password"]) return next(new Error("invalid username"));
       if (doc.deactivated) return next(new Error("user is deactivated"));
-
       bcrypt.compare(req.body.password, doc.password, (err, verifies) => {
         if (err) return next(err);
         if (!verifies) return next(new Error("invalid password"));
@@ -62,6 +75,59 @@ function App(db, secret) {
     });
   })
 
+  app.post('/signin', parseJSON, (req, res, next) => {
+
+    if (!checkBodyStructure(req.body, ['username', 'password', 'email'])) {
+      res.statusCode = 400;
+      res.end();
+      return
+    }
+
+    customerCollection.findOne({ email: req.body.email }, (err, doc) => {
+      if (!doc) {
+        // Add new user
+        bcrypt.hash(body.password, 10, (err, encrypted) => {
+
+          customerCollection.insertOne({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: encrypted,
+            username: req.body.username
+          }, (err) => {
+            if (err) return next(err);
+            // res.status(200).end();
+          });
+        })
+      } else {
+        /*
+        This is because, the costumer can order without registration.
+        But he would have to validate his email address every purchase 
+        */
+        if (doc["password"]) {
+          res.write('User already registered');
+          res.statusCode = 409;
+          res.end();
+          return;
+        }
+        bcrypt.hash(body.password, bcrypt.genSalt(), (err, encrypted) => {
+          customerCollection.updateOne({ _id: ObjectID(doc._id) }, {
+            '$set': {
+              name: req.body.name,
+              lastname: req.body.lastname,
+              password: encrypted,
+              username: req.body.username
+            }
+          })
+        })
+      }
+    })
+    res.end()
+
+  })
+
   return app;
+
 }
+
 module.exports = App;
